63d229ca6d66182fc714cdf412c504257a8ffd75
yarabramasta <yarabram111@gmail.com>
2022-12-01 16:58:52 +0700

    fix: remove register.ts from utils/mod.ts

--- utils/mod.ts
+++ utils/mod.ts
@@ -1,5 +1,4 @@
 export { default as endpoint } from "./endpoint.ts";
 export { default as params } from "./params.ts";
 export { default as querystr } from "./querystr.ts";
-export { default as register } from "./register.ts";
 export { default as response } from "./response.ts";
